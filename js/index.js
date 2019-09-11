6fb0360afeccb9637ea401c09b55e343c30534f4
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2019-09-11 16:46:27 +0530

    created first js file and some basic function

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.time('mycode took');
+console.log('hello ashutosh');
+console.log(4+32);
+console.log(true);
+console.log([32,12,43,65,77,55,90]);
+console.log({ashutosh : 'this', marks : 321});
+console.table({ashutosh : 'this', marks : 321});
+console.timeEnd('mycode took');
+console.assert(562>189, 'This is not possible')
+console.error('This is an error')
