c558736aff04274a3cb7c81248388da9b0fad2d1
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2019-09-16 11:41:26 +0530

    added tut3

---
+++ js/tut3.js
@@ -0,0 +1,59 @@
+console.log("tut3");
+
+
+//variable keyboard
+
+//var, let, const
+
+var name="ashutosh";
+var channel;
+var marks=10;
+channel=`willskhalifa`
+console.log(name, channel, marks);
+
+// Rules for creating javaScript variables
+
+/*
+
+1. cannot start with numbers.
+2. can start with letter, numbers, _ or $
+3. are case sensitive
+
+*/
+
+var city=`pune`
+console.log(city)
+
+const nerdname = `elliotenderson`
+
+
+console.log(nerdname)
+
+
+//let
+
+{
+    let city='mumbai';
+    //it will print only mumbai because of block
+    console.log(city);
+}
+
+console.log(city)
+
+
+const arr1 = [12,10,12,43,23,12]
+
+arr1.push(100)
+
+console.log(arr1)
+
+/* most common programming case types:
+
+1. CamelCase.
+2. kebab-case.
+3. snake_case.
+4. pascalCase
+
+
+*/
+
